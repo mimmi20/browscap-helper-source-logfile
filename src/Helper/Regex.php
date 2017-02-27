bd2d4c401322f7c4395503e04444fecc7f41bb7d
Thomas Müller <mimmi20@live.de>
2017-02-27 21:06:19 +0100

    update cs-fixer config

--- src/Helper/Regex.php
+++ src/Helper/Regex.php
@@ -1,25 +1,21 @@
 <?php
 /**
- * Copyright (c) 1998-2014 Browser Capabilities Project
+ * This file is part of the browscap-helper-source-logfile package.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * Copyright (c) 2016-2017, Thomas Mueller <mimmi20@live.de>
  *
- * Refer to the LICENSE file distributed with this package.
- *
- * @category   Browscap
- * @copyright  1998-2014 Browser Capabilities Project
- * @license    MIT
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
+declare(strict_types = 1);
 namespace BrowscapHelper\Source\Helper;
 
 /**
  * Class DiffCommand
  *
  * @category   Browscap
+ *
  * @author     James Titcumb <james@asgrim.com>
  */
 class Regex
@@ -30,23 +26,23 @@ class Regex
     public function getRegex()
     {
         return '/^'
-            . '(?P<remotehost>\S+)'             # remote host (IP)
+            . '(?P<remotehost>\S+)'             // remote host (IP)
             . '\s+'
-            . '(?P<logname>\S+)'                # remote logname
+            . '(?P<logname>\S+)'                // remote logname
             . '\s+'
-            . '(?P<user>\S+)'                   # remote user
+            . '(?P<user>\S+)'                   // remote user
             . '.*'
-            . '\[(?P<time>[^\]]+)\]'             # date/time
+            . '\[(?P<time>[^\]]+)\]'             // date/time
             . '[^"]+'
-            . '\"(?P<http>.*)\"'                # Verb(GET|POST|HEAD) Path HTTP Version
+            . '\"(?P<http>.*)\"'                // Verb(GET|POST|HEAD) Path HTTP Version
             . '\s+'
-            . '(?P<status>.*)'                  # Status
+            . '(?P<status>.*)'                  // Status
             . '\s+'
-            . '(?P<length>.*)'                  # Length (include Header)
+            . '(?P<length>.*)'                  // Length (include Header)
             . '[^"]+'
-            . '\"(?P<referrer>.*)\"'            # Referrer
+            . '\"(?P<referrer>.*)\"'            // Referrer
             . '[^"]+'
-            . '\"(?P<userAgentString>.*)\".*'   # User Agent
+            . '\"(?P<userAgentString>.*)\".*'   // User Agent
             . '$/x';
     }
 }
